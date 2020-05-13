08c4e04011c2304f3e793dec635d62387223eff4
perrylzx <46482518+perrylzx@users.noreply.github.com>
2020-05-14 06:33:49 +0800

    render data

---
+++ frontend/src/components/Visualizer.js
@@ -0,0 +1,15 @@
+import FileAttachments from "@observablehq/stdlib/src/fileAttachment";
+import React from "react";
+import Dataset from "../dataset/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
+export default class Visualizer extends React.Component {
+  render() {
+    const text = FileAttachments(Dataset);
+    console.log(text);
+    return (
+      <div>
+        <h1>{text}</h1>
+      </div>
+    );
+  }
+}
