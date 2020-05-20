5cfb5096ab9f0424cda8d7f9df32f0ac91c14ad7
perrylzx <46482518+perrylzx@users.noreply.github.com>
2020-05-20 09:01:54 +0800

    wrote code in data fetch api to parse csv and send it in response

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,8 @@ var cookieParser = require("cookie-parser");
 var indexRouter = require("./routes/index");
 var logger = require("morgan");
 var path = require("path");
-import download from "download-git-repo";
+import fs from "fs";
+import papa from "papaparse";
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -12,11 +13,33 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use("/download", fetchData);
+// todo(perry): make cron job to perform this api call automatically
 app.get("/download", (req, res) => {
-  download("CSSEGISandData/COVID-19", "../frontend/src/dataset", (err) => {
-    res.send(err ? err : "Success");
+  download("CSSEGISandData/COVID-19", "./src/dataset", (err) => {
+    console.log(err ? err : "Success");
   });
+  const chunks = [];
+  fs.createReadStream(
+    path.resolve(
+      __dirname,
+      "dataset",
+      "csse_covid_19_data",
+      "csse_covid_19_time_series",
+      "time_series_covid19_confirmed_global.csv"
+    )
+  )
+    .pipe(
+      papa.parse(papa.NODE_STREAM_INPUT, {
+        dynamicTyping: true,
+        delimiter: ",",
+        header: true,
+      })
+    )
+    .on("error", (error) => console.error(error))
+    // on data event, every append every chunk of data to chunks array
+    .on("data", (chunk) => chunks.push(chunk))
+    // on end event, send completed chunks array
+    .on("end", () => res.send(chunks));
 });
 
 module.exports = app;
