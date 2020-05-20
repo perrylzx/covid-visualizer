2c8c897cb8a3bf345141949414f8902e84d1d47f
perrylzx <46482518+perrylzx@users.noreply.github.com>
2020-05-20 09:15:32 +0800

    moved api controller to routes folder

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,9 @@
-var express = require("express");
-var app = express();
-var cookieParser = require("cookie-parser");
-var indexRouter = require("./routes/index");
-var logger = require("morgan");
-var path = require("path");
-import fs from "fs";
-import papa from "papaparse";
+import cookieParser from "cookie-parser";
+import express from "express";
+import logger from "morgan";
+import path from "path";
+import fetchData from "./routes/fetchData";
+const app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -14,32 +12,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 // todo(perry): make cron job to perform this api call automatically
-app.get("/download", (req, res) => {
-  download("CSSEGISandData/COVID-19", "./src/dataset", (err) => {
-    console.log(err ? err : "Success");
-  });
-  const chunks = [];
-  fs.createReadStream(
-    path.resolve(
-      __dirname,
-      "dataset",
-      "csse_covid_19_data",
-      "csse_covid_19_time_series",
-      "time_series_covid19_confirmed_global.csv"
-    )
-  )
-    .pipe(
-      papa.parse(papa.NODE_STREAM_INPUT, {
-        dynamicTyping: true,
-        delimiter: ",",
-        header: true,
-      })
-    )
-    .on("error", (error) => console.error(error))
-    // on data event, every append every chunk of data to chunks array
-    .on("data", (chunk) => chunks.push(chunk))
-    // on end event, send completed chunks array
-    .on("end", () => res.send(chunks));
-});
+app.use("/download", fetchData);
 
 module.exports = app;
--- backend/src/routes/fetchData.js
+++ backend/src/routes/fetchData.js
@@ -1,10 +1,37 @@
 var express = require("express");
 var router = express.Router();
+import download from "download-git-repo";
+import fs from "fs";
+import papa from "papaparse";
+import path from "path";
 
 /* GET home page. */
-router.get("/download", (req, res) => {
-  console.log("asb");
-  res.send("asdf");
+router.get("/", async (req, res) => {
+  await download("CSSEGISandData/COVID-19", "./src/dataset", (err) => {
+    console.log(err ? err : "Success");
+  });
+  const chunks = [];
+  fs.createReadStream(
+    path.resolve(
+      "src",
+      "dataset",
+      "csse_covid_19_data",
+      "csse_covid_19_time_series",
+      "time_series_covid19_confirmed_global.csv"
+    )
+  )
+    .pipe(
+      papa.parse(papa.NODE_STREAM_INPUT, {
+        dynamicTyping: true,
+        delimiter: ",",
+        header: true,
+      })
+    )
+    .on("error", (error) => console.error(error))
+    // on data event, every append every chunk of data to chunks array
+    .on("data", (chunk) => chunks.push(chunk))
+    // on end event, send completed chunks array
+    .on("end", () => res.send(chunks));
 });
 
 module.exports = router;
