95c371bfbb8e91c603f063c163925e9fd4ce75a9
perrylzx <46482518+perrylzx@users.noreply.github.com>
2020-05-30 04:58:13 +0800

    fixed visualisation not rendering path correctly

--- frontend/src/components/CovidVisualizer.js
+++ frontend/src/components/CovidVisualizer.js
@@ -7,27 +7,23 @@ export default class CovidVisualizer extends React.Component {
     this.myRef = React.createRef();
     this.state = {};
   }
-  //todo (perry) : path not being drawn. only svg and x and y axis being rendered. find out why
   drawChart() {
     const dates = [];
     const cases = [];
+    const pathData = [];
+
     var margin = { top: 20, right: 20, bottom: 30, left: 50 },
       width = 960 - margin.left - margin.right,
       height = 500 - margin.top - margin.bottom;
 
     //format the data
     const parseTime = d3.timeParse("%m/%e/%y");
-    // define the 1st line
-    const valueline = d3
-      .line()
-      // this uses the d3.scaleTime function in the variable x on the array of dates
-      .x((d) => {
-        return x(dates);
-      })
-      .y((d) => {
-        return y(cases);
-      });
 
+    // set the ranges
+    var x = d3.scaleTime().range([0, width]);
+    var y = d3.scaleLinear().range([height, 0]);
+
+    // format data and push dates and case numbers to array
     this.state.covidCases.forEach((country) => {
       if (country.hasOwnProperty("Singapore")) {
         this.Singapore = country.Singapore;
@@ -35,46 +31,47 @@ export default class CovidVisualizer extends React.Component {
           key = parseTime(key);
           dates.push(key);
           cases.push(value);
+          const datesToCases = { date: key, cases: value };
+          pathData.push(datesToCases);
         }
       }
     });
-    // set the ranges
-    var x = d3.scaleTime().range([0, width]);
-    var y = d3.scaleLinear().range([height, 0]);
 
-    // scale the range of the data
-    x.domain(
-      d3.extent(this.Singapore, (d) => {
-        return dates;
+    // define the 1st line
+    const valueline = d3
+      .line()
+      // this uses the d3.scaleTime function in the variable x on the array of dates
+      .x((d) => {
+        return x(d.date); // this might be the problem. maybe this function requires to look at the dictionary data and not from the array as it needs to correlate the case to the date
       })
-    );
-    y.domain([
-      0,
-      d3.max(this.Singapore, (d) => {
-        for (let [key, value] of Object.entries(this.Singapore)) {
-          console.log("hereererer" + this.Singapore[key]);
-          return Math.max(value);
-        }
-      }),
-    ]);
+      .y((d) => {
+        return y(d.cases);
+      });
+
     // appending svg to ref and a group element
     const node = d3
       .select(this.myRef.current)
       .append("svg")
+      .style("border-style", "solid")
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
       .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+    // scale the range of the data
+    x.domain(d3.extent(dates));
+    y.domain([0, d3.max(cases)]);
+
     //add the singapore path
     node
       .append("path")
-      .data([this.Singapore])
+      .data([pathData])
       .attr("class", "line")
       .attr("d", valueline)
       .style("stroke-width", "2px")
       .style("stroke", "steelblue")
       .style("fill", "none");
+
     // Add the X Axis
     node
       .append("g")
