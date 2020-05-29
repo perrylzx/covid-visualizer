bd66ecd5e32b0ebdfebde664c88b7eb582222368
perrylzx <46482518+perrylzx@users.noreply.github.com>
2020-05-30 05:16:39 +0800

    imported bootstrap stylesheet and display dropdown for country selector

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
+import CountrySelector from "./components/CountrySelector";
 import CovidVisualizer from "./components/CovidVisualizer";
 
 function App() {
   return (
     <div>
+      <CountrySelector />
       <CovidVisualizer />
     </div>
   );
