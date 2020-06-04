b3b24fd6cd69941b7ad5b1c3168a92e8f987d7c6
perrylzx <46482518+perrylzx@users.noreply.github.com>
2020-06-04 08:21:39 +0800

    created a text component that explains what the dropdown does

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import CovidVisualizer from "./components/CovidVisualizer";
+import Introduction from "./components/Introduction";
 
 function App() {
   return (
     <div>
+      <Introduction />
       <CovidVisualizer />
     </div>
   );
---
+++ frontend/src/components/Introduction.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default class Introduction extends React.Component {
+  render() {
+    return <h3>Select the country to display Covid-19 growth</h3>;
+  }
+}
