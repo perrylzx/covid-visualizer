4f522d68abfa874d6100d6db9e531e00f7fa11d2
perrylzx <perrylzx@gmail.com>
2020-07-15 16:22:30 +0800

    made introduction centered

--- frontend/src/components/Introduction.js
+++ frontend/src/components/Introduction.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export default class Introduction extends React.Component {
   render() {
-    return <h3>Select the country to display Covid-19 growth</h3>;
+    return <h3 style={{textAlign: 'center'}}>Select the country to display Covid-19 growth</h3>;
   }
 }
