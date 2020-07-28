95e58b3b525bf88ecd9d855e6533abe5517539ac
perrylzx <46482518+perrylzx@users.noreply.github.com>
2020-07-28 18:38:25 +0800

    changed font

--- frontend/src/components/Introduction.js
+++ frontend/src/components/Introduction.js
@@ -2,6 +2,10 @@ import React from "react";
 
 export default class Introduction extends React.Component {
   render() {
-    return <h3>Select the country to display Covid-19 growth</h3>;
+    return (
+      <h3 style={{ textAlign: "center", fontFamily: "fira-mono-medium" }}>
+        Select the country to display Covid-19 growth
+      </h3>
+    );
   }
 }
